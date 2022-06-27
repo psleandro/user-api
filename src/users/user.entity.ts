e63a355dfdc57561128118558bafc2ad65778f8f
Leandro Santos <lsaantos@outlook.com>
2022-06-26 22:03:37 -0300

    feat: add class validator to user entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,8 +1,30 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class User {
   id: number;
+
+  @IsNotEmpty({ message: 'username field is mandatory' })
+  @IsString({
+    message: 'username need be a string',
+  })
   username: string;
+
+  @IsEmail(
+    {},
+    {
+      message: 'email field need be send with correctly syntax',
+    },
+  )
   email: string;
-  senha: string;
+
+  @IsNotEmpty({ message: 'password field is mandatory' })
+  password: string;
+
+  @IsNotEmpty({
+    message: 'fullname field is mandatory',
+  })
+  @IsString()
   fullname: string;
+
   enterDate: Date;
 }
